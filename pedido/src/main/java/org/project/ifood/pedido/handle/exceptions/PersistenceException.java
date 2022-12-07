e2ebed33a9a722d6d0a544d13c6675e0868a2432
Lelton Borges <leltondev@gmail.com>
2022-12-06 21:04:56 -0300

    chore(Handle): custom exception

---
+++ pedido/src/main/java/org/project/ifood/pedido/handle/exceptions/PersistenceException.java
@@ -0,0 +1,15 @@
+package org.project.ifood.pedido.handle.exceptions;
+
+import java.io.Serializable;
+
+public class PersistenceException extends RuntimeException implements Serializable {
+    private static final long serialVersionUID = 4852273032613321858L;
+
+    public PersistenceException(String message) {
+        super(message);
+    }
+
+    public PersistenceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- pedido/src/main/java/org/project/ifood/pedido/kafka/incoming/PedidoIncoming.java
+++ pedido/src/main/java/org/project/ifood/pedido/kafka/incoming/PedidoIncoming.java
@@ -3,12 +3,12 @@ package org.project.ifood.pedido.kafka.incoming;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Message;
 import org.project.ifood.pedido.dto.pedido.PedidoRealizadoDTO;
+import org.project.ifood.pedido.handle.exceptions.PersistenceException;
 import org.project.ifood.pedido.mapper.PedidoMapper;
 import org.project.ifood.pedido.model.Pedido;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.persistence.PersistenceException;
 import javax.transaction.Transactional;
 import java.util.concurrent.CompletionStage;
 
